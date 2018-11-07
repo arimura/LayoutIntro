d668be9f586a34c020093305ca37ed1a9769d0f9
Kotaro Arimura <kotaro_arimura@voyagegroup.com>
2018-11-07 19:12:33 +0900

    Show vc programmatically

---
+++ LayoutIntro/HomeTableViewController.h
@@ -0,0 +1,17 @@
+//
+//  HomeTableViewController.h
+//  LayoutIntro
+//
+//  Created by 有村 皓太郎 on 2018/11/07.
+//  Copyright © 2018年 hormiga6. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HomeTableViewController : UITableViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ LayoutIntro/HomeTableViewController.m
@@ -0,0 +1,46 @@
+//
+//  HomeTableViewController.m
+//  LayoutIntro
+//
+//  Copyright © 2018年 hormiga6. All rights reserved.
+//
+
+#import "HomeTableViewController.h"
+#import "RotateViewController.h"
+#import "AnchorViewController.h"
+
+@interface HomeTableViewController ()
+@property (nonatomic) NSArray *classNameList;
+@end
+
+@implementation HomeTableViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.classNameList = @[
+                           NSStringFromClass([RotateViewController class]),
+                           NSStringFromClass([AnchorViewController class]),
+                           ];
+}
+
+#pragma mark - Table view data source
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return [self.classNameList count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"homeItem" forIndexPath:indexPath];
+    [cell.textLabel setText:self.classNameList[indexPath.row]];
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    UITableViewCell *cell =  [tableView cellForRowAtIndexPath:indexPath];
+    UIViewController *vc = [[NSClassFromString(cell.textLabel.text) alloc] init];
+    [self presentViewController:vc animated:YES completion:nil];
+}
+@end
