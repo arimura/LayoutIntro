ee8ad7cecd0fc62f38c6170f656b15ae13aedaaa
Kotaro Arimura <kotaro_arimura@voyagegroup.com>
2018-11-07 20:35:45 +0900

    Add rotation sample

--- LayoutIntro/RotateViewController.m
+++ LayoutIntro/RotateViewController.m
@@ -9,6 +9,8 @@
 
 @interface RotateViewController ()
 @property (nonatomic) UIView *containerView;
+@property (nonatomic) NSArray<NSLayoutConstraint*> *portraitConstraints;
+@property (nonatomic) NSArray<NSLayoutConstraint*> *landscapeConstraints;
 @end
 
 @implementation RotateViewController
@@ -21,58 +23,53 @@
     self.containerView.backgroundColor = [UIColor blueColor];
     [self.view addSubview:self.containerView];
     NSLog(@"viewDidLoad");
+    
     if(@available(iOS 11, *)){
-        NSLog(@"use safearea layout guide");
         __auto_type guide = self.view.safeAreaLayoutGuide;
-        [self.containerView.topAnchor constraintEqualToAnchor:guide.topAnchor].active = true;
-        [self.containerView.leadingAnchor constraintEqualToAnchor:self.view.leadingAnchor].active = true;
-        [self.containerView.trailingAnchor constraintEqualToAnchor:self.view.trailingAnchor].active = true;
-        [self.containerView.bottomAnchor constraintEqualToAnchor:self.view.bottomAnchor].active = true;
-    }else{
-        [self setLayout1];
+        self.portraitConstraints = @[
+                                     [self.containerView.topAnchor constraintEqualToAnchor:guide.topAnchor],
+                                     [self.containerView.leadingAnchor constraintEqualToAnchor:self.view.leadingAnchor],
+                                     [self.containerView.trailingAnchor constraintEqualToAnchor:self.view.trailingAnchor],
+                                     [self.containerView.bottomAnchor constraintEqualToAnchor:self.view.bottomAnchor]
+                                     ];
+        self.landscapeConstraints = @[
+                                     [self.containerView.topAnchor constraintEqualToAnchor:self.view.topAnchor],
+                                     [self.containerView.leadingAnchor constraintEqualToAnchor:guide.leadingAnchor],
+                                     [self.containerView.trailingAnchor constraintEqualToAnchor:guide.trailingAnchor],
+                                     [self.containerView.bottomAnchor constraintEqualToAnchor:self.view.bottomAnchor]
+                                     ];
     }
 }
 
-- (void)setLayout1{
-    NSLayoutConstraint *top =[NSLayoutConstraint
-                              constraintWithItem:self.view
-                              attribute:NSLayoutAttributeTop
-                              relatedBy:NSLayoutRelationEqual
-                              toItem:self.containerView
-                              attribute:NSLayoutAttributeTop
-                              multiplier:1
-                              constant:0];
-    [self.view addConstraint:top];
-
-    NSLayoutConstraint *leading =[NSLayoutConstraint
-                              constraintWithItem:self.view
-                              attribute:NSLayoutAttributeLeading
-                              relatedBy:NSLayoutRelationEqual
-                              toItem:self.containerView
-                              attribute:NSLayoutAttributeLeading
-                              multiplier:1
-                              constant:0];
-    [self.view addConstraint:leading];
-    
-    NSLayoutConstraint *trailing =[NSLayoutConstraint
-                                  constraintWithItem:self.containerView
-                                  attribute:NSLayoutAttributeTrailing
-                                  relatedBy:NSLayoutRelationEqual
-                                  toItem:self.view
-                                  attribute:NSLayoutAttributeTrailing
-                                  multiplier:1
-                                  constant:0];
-    [self.view addConstraint:trailing];
-
-    NSLayoutConstraint *bottom =[NSLayoutConstraint
-                                   constraintWithItem:self.view
-                                   attribute:NSLayoutAttributeBottom
-                                   relatedBy:NSLayoutRelationEqual
-                                   toItem:self.containerView
-                                   attribute:NSLayoutAttributeBottom
-                                   multiplier:1
-                                   constant:0];
-    [self.view addConstraint:bottom];
+- (void)setLegacyLayout{
+    [NSLayoutConstraint constraintWithItem:self.containerView
+                                 attribute:NSLayoutAttributeTop
+                                 relatedBy:NSLayoutRelationEqual
+                                    toItem:self.topLayoutGuide
+                                 attribute:NSLayoutAttributeBottom
+                                multiplier:1
+                                  constant:0].active = YES;
+    [NSLayoutConstraint constraintWithItem:self.view
+                                 attribute:NSLayoutAttributeLeading
+                                 relatedBy:NSLayoutRelationEqual
+                                    toItem:self.containerView
+                                 attribute:NSLayoutAttributeLeading
+                                multiplier:1
+                                  constant:0].active = YES;
+    [NSLayoutConstraint constraintWithItem:self.containerView
+                                 attribute:NSLayoutAttributeTrailing
+                                 relatedBy:NSLayoutRelationEqual
+                                    toItem:self.view
+                                 attribute:NSLayoutAttributeTrailing
+                                multiplier:1
+                                  constant:0].active = YES;
+    [NSLayoutConstraint constraintWithItem:self.view
+                                 attribute:NSLayoutAttributeBottom
+                                 relatedBy:NSLayoutRelationEqual
+                                    toItem:self.containerView
+                                 attribute:NSLayoutAttributeBottom
+                                multiplier:1
+                                  constant:0].active = YES;
 }
 
 
@@ -81,6 +78,19 @@
 - (void)traitCollectionDidChange:(UITraitCollection *)previousTraitCollection {
     NSLog(@"previous %@", previousTraitCollection);
     NSLog(@"current %@", self.traitCollection);
-    [self setLayout1];
+    
+    if(@available(iOS 11, *)){
+        if(self.traitCollection.verticalSizeClass == UIUserInterfaceSizeClassCompact){
+            NSLog(@"=== Landscape ");
+            [NSLayoutConstraint deactivateConstraints:self.portraitConstraints];
+            [NSLayoutConstraint activateConstraints:self.landscapeConstraints];
+        }else{
+            NSLog(@"=== Portrait ");
+            [NSLayoutConstraint deactivateConstraints:self.landscapeConstraints];
+            [NSLayoutConstraint activateConstraints:self.portraitConstraints];
+        }
+    }else{
+        [self setLegacyLayout];
+    }
 }
 @end
